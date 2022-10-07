48bf957a6ddf7778160f517476f232513527e3a9
Francisco <fcaro@switch.cl>
2022-10-07 15:06:24 -0300

    connected via physical tablet device.  Compiles and loads PDF from /sdcard/Downloads
    BT: with method that PairsDevice. Outputs list of all pairedDevices to terminal.
    
    NB.  pairedDevice is not the same as ScannedDevices.
    user needs to BT connect outside of app, before app successfully lists all pairedDevices.

--- app/src/main/java/com/fcaro/ebookreaderoverdualtablets/MainActivity.java
+++ app/src/main/java/com/fcaro/ebookreaderoverdualtablets/MainActivity.java
@@ -2,12 +2,19 @@ package com.fcaro.ebookreaderoverdualtablets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
@@ -15,10 +22,27 @@ import com.github.barteksc.pdfviewer.PDFView;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     PDFView pdfView;
     Button simpleButton1, simpleButton2;
+    //private static final UUID MY_UUID_INSECURE =
+    //        UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothDevice mmDevice;
+    private UUID deviceUUID;
+    //ConnectedThread mConnectedThread;
+    private Handler handler;
+
+    String TAG = "MainActivity";
+    EditText send_data;
+    TextView view_data;
+    StringBuilder messages;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,5 +74,36 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    public void pairDevice(View v) {
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        Log.e("MAinActivity", "" + pairedDevices.size() );
+        if (pairedDevices.size() > 0) {
+            Object[] devices = pairedDevices.toArray();
+            BluetoothDevice device = (BluetoothDevice) devices[0];
+            ParcelUuid[] uuid = device.getUuids();
+            
+            for ( Object item : devices){
+                Log.e("MAinActivity :: pairDevice() ::: device :", ""
+                        + (BluetoothDevice) item );
+                Log.e("MAinActivity :: pairDevice() ::: device.getName() :", ""
+                        + ((BluetoothDevice) item).getName() );
+                ParcelUuid[] uuidTemp = ((BluetoothDevice) item).getUuids();
+                Log.e("MAinActivity :: pairDevice() ::: uuid : ", ""
+                        + ((BluetoothDevice) item).getUuids().toString() );
+            }
+            //ConnectThread connect = new ConnectThread(device,MY_UUID_INSECURE);
+            //connect.start();
+
+        }
     }
 }
